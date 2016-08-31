bfbda7f91063e4e6cb2e5446fbf4153d02adc276
Robin Bader <robin.bader@forcklabs.com>
2016-08-31 16:52:51 +0200

    Calculate Cell Positions and with according to screen size

--- src/main/scala/HexagonalGrid/Layout.scala
+++ src/main/scala/HexagonalGrid/Layout.scala
@@ -1,14 +1,14 @@
 package HexagonalGrid
 
-case class Layout(orientation: Orientation, size: Point, origin: Point) {
+case class Layout(orientation: Orientation, size: Double, origin: Point) {
   def hexToPixel(h: Hex): Point = {
-    val x = (orientation.f0 * h.q + orientation.f1 * h.r) * size.x
-    val y = (orientation.f2 * h.q + orientation.f3 * h.r) * size.y
+    val x = (orientation.f0 * h.q + orientation.f1 * h.r) * size
+    val y = (orientation.f2 * h.q + orientation.f3 * h.r) * size
     Point(x + origin.x, y + origin.y)
   }
 
   def pixelToHex(p: Point): FractionalHex = {
-    val point = Point((p.x - origin.x) / size.x, (p.y - origin.y) / size.y)
+    val point = Point((p.x - origin.x) / size, (p.y - origin.y) / size)
     val q: Double = orientation.b0 * point.x + orientation.b1 * point.y
     val r: Double = orientation.b2 * point.x + orientation.b3 * point.y
     FractionalHex(q, r, -q - r) // TODO: refactor with constructor overlaoding. Remove -q-r
@@ -16,7 +16,7 @@ case class Layout(orientation: Orientation, size: Point, origin: Point) {
 
   def hexCornerOffset(corner: Int): Point = {
     val angle: Double = 2 * math.Pi * (orientation.startAngle + corner) / 6
-    Point(size.x * math.cos(angle), size.y * math.sin(angle))
+    Point(size * math.cos(angle), size * math.sin(angle))
   }
 
   def polygonCorners(h: Hex): Vector[Point] = {
--- src/main/scala/MainApp.scala
+++ src/main/scala/MainApp.scala
@@ -7,35 +7,47 @@ class Main extends PApplet {
   var hexMap: HexMap = _
 
   override def settings(): Unit = {
-    size(1200, 1200)
+    size(1200, 400)
   }
 
-  override def setup(): Unit = {
-    val orientation = HexOrientation.Pointy
-    val size = Point(40, 40)
-    val center = Point(200, 200)
+  def sizeToWidth(size: Double): Double = 1 / 2 * math.sqrt(3) * math.sqrt(size * size)
+  def widthToSize(width: Double): Double = 2 * width / math.sqrt(3)
 
+  override def setup(): Unit = {
     hexMap = MapBuilder.createMap(System.getProperty("user.dir") + "/src/resources/maps/map1.txt")
+    val margin = 10
+
+
+    val w = (width - 2 * margin) / (hexMap.width + 0.5)
+    val h = (height - 2 * margin) / (hexMap.height + 7 / 8)
+    val cellWidth = math.min(w, h)
+    val size = widthToSize(cellWidth  / 2)
+
+
+    val orientation = HexOrientation.Pointy
+    val center = Point(cellWidth / 2 + margin, size + margin)
+
     layout = Layout(orientation, size, center)
 
     background(255)
     for (i <- 0 until hexMap.height) {
       for (j <- 0 until hexMap.width) {
-        val offsetCoord = OffsetCoord(i, j)
+        val offsetCoord = OffsetCoord(j, i)
         val hex: Hex = offsetCoord.offsetToCube
         val cell: Cell = hexMap.get(offsetCoord)
         val center = layout.hexToPixel(hex)
         val corners: Vector[Point] = layout.polygonCorners(hex)
+        println(corners.length)
 
         cell.state match {
-          case Wall => fill(0)
+          case Wall => fill(128)
           case Empty => fill(255)
         }
 
         stroke(0)
         beginShape()
         corners.foreach(c => vertex(c.x.toFloat, c.y.toFloat))
-        endShape()
+        endShape(2) // Mode close == 2
 
         textSize(10)
         fill(0)
